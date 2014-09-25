e20294b35e95d58b9178b12f3656b8e455e6ce71
Crystalix <e.gillespie42@hotmail.com>
2014-09-25 16:30:35 +0100

    added sdl dll and edited dependencies

---
+++ GamesProgramming2labs/GamesProgramming2labs/main.cpp
@@ -0,0 +1,22 @@
+//header files
+
+#include <iostream>
+
+//global variables go here
+
+void InitWindow(int width, int height, bool fullscreen)
+{
+
+}
+
+void CleanUp()
+{
+
+}
+
+//Main method-entry point
+
+int main(int argc, char * arg[])
+{
+	return 0;
+}
